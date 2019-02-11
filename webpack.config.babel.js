374b8ae527ac5acd3823f73807c4f0b2838e9a00
AndersDJohnson <anders.d.johnson.developer@gmail.com>
2019-02-10 20:04:42 -0600

    chore: fix demo and add publish script

--- webpack.config.babel.js
+++ webpack.config.babel.js
@@ -2,10 +2,12 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const nodeExternals = require('webpack-node-externals')
 
 module.exports = (env = {}) => ({
-  target: 'node',
-  output: {
-    libraryTarget: 'commonjs2'
-  },
+  target: env.demo ? undefined : 'node',
+  output: env.demo
+    ? undefined
+    : {
+        libraryTarget: 'commonjs2'
+      },
   module: {
     rules: [
       {
@@ -19,5 +21,9 @@ module.exports = (env = {}) => ({
     minimize: false
   },
   externals: [env.demo ? null : nodeExternals()].filter(Boolean),
-  plugins: [new HtmlWebpackPlugin()]
+  plugins: [
+    new HtmlWebpackPlugin({
+      title: 'react-redux-connect-keyed-ownprops example'
+    })
+  ]
 })
