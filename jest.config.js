7102b787fcac8dcab23e5e78062f12530fc9ddc4
AndersDJohnson <anders.d.johnson.developer@gmail.com>
2019-02-10 19:37:43 -0600

    chore: add scripts for CI

--- jest.config.js
+++ jest.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   collectCoverage: true,
+  coverageThreshold: {
+    global: {
+      branches: 100,
+      functions: 100,
+      lines: 100,
+      statements: 100
+    }
+  },
   setupFilesAfterEnv: ['enzyme-adapter-configure'],
   snapshotSerializers: ['enzyme-to-json/serializer']
 }
